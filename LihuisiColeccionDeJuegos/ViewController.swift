b2b7d755643a9b685ed1cac3abba54dc8b42e019
Mac 01 <eduardo.lihuisi@tecsup.edu.pe>
2023-05-17 09:00:36 -0500

    Initial Commit

---
+++ LihuisiColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LihuisiColeccionDeJuegos
+//
+//  Created by Mac 01 on 17/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
